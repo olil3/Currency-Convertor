37d91b56420db434d437d03d483d80a88fe49b56
Lalit Sriram Varma Manthena <smvarma2@illinois.edu>
2020-05-04 07:56:45 -0500

    made the opening screen and created the intent from opening screen to main activity

--- app/src/main/java/com/leboys/currencyconverter/opening_screen.java
+++ app/src/main/java/com/leboys/currencyconverter/opening_screen.java
@@ -2,13 +2,26 @@ package com.leboys.currencyconverter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class opening_screen extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opening_screen);
+        button = findViewById(R.id.enterButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(opening_screen.this, MainActivity.class);
+                startActivity(intent1);
+            }
+        });
     }
 }
